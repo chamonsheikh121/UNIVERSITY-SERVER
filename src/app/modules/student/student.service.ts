47531aa3055bd2c3e2d85eaedd05ff7bd013dd46
chamonsheikh121 <studentchamon1212@gmail.com>
2025-09-01 23:58:21 +0600

    validation problem fixed

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -26,3 +26,32 @@ export const studentServices = {
   getAllStudentsFromDB,
   getStudentFromDB,
 };
+
+import { TStudent } from './student.interface';
+import StudentModel from './student.model';
+
+const createStudentToDB = async (student_data: TStudent) => {
+
+
+  if (await StudentModel.is_user_email_exist(student_data.email)) {
+    throw new Error('User already exist from statics ! ');
+  }
+
+  const result = await StudentModel.create(student_data);
+  return result;
+};
+
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+const getStudentFromDB = async (id: string) => {
+  const result = await StudentModel.findOne({ id });
+  return result;
+};
+
+export const studentServices = {
+  createStudentToDB,
+  getAllStudentsFromDB,
+  getStudentFromDB,
+};
