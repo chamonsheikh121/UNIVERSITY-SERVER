4f3e9aea01ec7027c01da544fb78583ac23f3a8c
chamonsheikh121 <studentchamon1212@gmail.com>
2025-09-30 03:09:25 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ app.use(express.text());
 app.use(cooke_parser());
 app.use(
   cors({
-    origin: ['http://localhost:5173/'],
+    origin: ['http://localhost:5173'],
+    credentials: true,
   }),
 );
 
